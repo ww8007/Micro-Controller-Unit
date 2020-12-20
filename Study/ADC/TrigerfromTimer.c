c77f4e86a5def6591e15f88566e61c41f2a3da32
ww8007 <wshmin@naver.com>
2020-12-20 23:52:47 +0900

    ADC, Timer 수정완료

--- Study/ADC/TrigerfromTimer.c
+++ Study/ADC/TrigerfromTimer.c
@@ -64,100 +64,100 @@ int main(void)
 }
 
 void _ADC_Init(void)
-{   	// ADC1: PA1(pin 41)
+{   	// ADC2: PA1(pin 41)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;	// (1<<0) ENABLE GPIOA CLK (stm32f4xx.h 참조)
 	GPIOA->MODER |= (3<<2*1);		// CONFIG GPIOA PIN1(PA1) TO ANALOG IN MODE
 						
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;	// (1<<8) ENABLE ADC1 CLK (stm32f4xx.h 참조)
+	RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;	// (1<<8) ENABLE ADC1 CLK (stm32f4xx.h 참조)
 
 	ADC->CCR &= ~(0X1F<<0);		// MULTI[4:0]: ADC_Mode_Independent
 	ADC->CCR |=  (1<<16); 		// 0x00010000 ADCPRE:ADC_Prescaler_Div4 (ADC MAX Clock 36MHz, 84Mhz(APB2)/4 = 21MHz)
         
-	ADC1->CR1 &= ~(3<<24);		// RES[1:0]=0b00 : 12bit Resolution
-	ADC1->CR1 &= ~(1<<8);		// SCAN=0 : ADC_ScanCovMode Disable
-	ADC1->CR1 |=  (1<<5);		// EOCIE=1: Interrupt enable for EOC
+	ADC2->CR1 &= ~(3<<24);		// RES[1:0]=0b00 : 12bit Resolution
+	ADC2->CR1 &= ~(1<<8);		// SCAN=0 : ADC_ScanCovMode Disable
+	ADC2->CR1 |=  (1<<5);		// EOCIE=1: Interrupt enable for EOC
 
-	ADC1->CR2 &= ~(1<<1);		// CONT=0: ADC_Continuous ConvMode Disable
-	ADC1->CR2 |=  (2<<28);		// EXTEN[1:0]: ADC_ExternalTrigConvEdge_Enable(Falling Edge)
-	ADC1->CR2 |= (0x0C<<24);	// EXTSEL[3:0]: ADC_ExternalTrig (TIM5_CC3)
-	ADC1->CR2 &= ~(1<<11);		// ALIGN=0: ADC_DataAlign_Right
-	ADC1->CR2 &= ~(1<<10);		// EOCS=0: The EOC bit is set at the end of each sequence of regular conversions
+	ADC2->CR2 &= ~(1<<1);		// CONT=0: ADC_Continuous ConvMode Disable
+	ADC2->CR2 |=  (2<<28);		// EXTEN[1:0]: ADC_ExternalTrigConvEdge_Enable(Falling Edge)
+	ADC2->CR2 |= (0X07<<24);	// EXTSEL[3:0]: ADC_ExternalTrig (TIM3_CC1)
+	ADC2->CR2 &= ~(1<<11);		// ALIGN=0: ADC_DataAlign_Right
+	ADC2->CR2 &= ~(1<<10);		// EOCS=0: The EOC bit is set at the end of each sequence of regular conversions
 
-	ADC1->SQR1 &= ~(0xF<<20);	// L[3:0]=0b0000: ADC Regular channel sequece length 
+	ADC2->SQR1 &= ~(0xF<<20);	// L[3:0]=0b0000: ADC Regular channel sequece length 
 					// 0b0000:1 conversion)
- 	//Channel selection, The Conversion Sequence of PIN1(ADC1_CH1) is first, Config sequence Range is possible from 0 to 17
-	ADC1->SQR3 |= (1<<0);		// SQ1[4:0]=0b0001 : CH1
-        ADC1->SMPR2 |= (0x7<<(3*1));	// ADC1_CH1 Sample TIme_480Cycles (3*Channel_1)
- 	//Channel selection, The Conversion Sequence of PIN1(ADC1_CH1) is first, Config sequence Range is possible from 0 to 17
+ 	//Channel selection, The Conversion Sequence of PIN1(ADC2_CH1) is first, Config sequence Range is possible from 0 to 17
+	ADC2->SQR3 |= (1<<0);		// SQ1[4:0]=0b0001 : CH1
+    ADC2->SMPR2 |= (0x7<<(3*1));	// ADC2_CH1 Sample TIme_480Cycles (3*Channel_1)
+ 	//Channel selection, The Conversion Sequence of PIN1(ADC2_CH1) is first, Config sequence Range is possible from 0 to 17
 
 	NVIC->ISER[0] |= (1<<18);	// Enable ADC global Interrupt
 
-	ADC1->CR2 |= (1<<0);		// ADON: ADC ON
+	ADC2->CR2 |= (1<<0);		// ADON: ADC ON
 }
 
-void TIMER5_Init(void)
+void TIMER3_Init(void)
 {
 // TIM5_CH3 (PA2) : 500ms 인터럽트 발생
-// Clock Enable : GPIOA & TIMER5
-	RCC->AHB1ENR	|= (1<<0);	// GPIOA Enable
+// Clock Enable : GPIOA & TIMER5RCC->AHB1ENR	|= (1<<0);	// GPIOA Enable
+	
 	RCC->APB1ENR 	|= (1<<3);	// TIMER5 Enable 
     						
 // PA2을 출력설정하고 Alternate function(TIM5_CH3)으로 사용 선언 
-	GPIOA->MODER 	|= (2<<2*2);	// PA2 Output Alternate function mode					
-	GPIOA->OSPEEDR 	|= (3<<2*2);	// PA2 Output speed (100MHz High speed)
-	GPIOA->OTYPER	&= ~(1<<2);	// PA2 Output type push-pull (reset state)
-	GPIOA->AFR[0]	|= (2<<8); 	// 0x00000200	(AFR[0].(11~8)=0b0010): Connect TIM5 pins(PA2) to AF2(TIM3..5)
-					// PA2 ==> TIM5_CH3
+	// GPIOA->MODER 	|= (2<<2*2);	// PA2 Output Alternate function mode					
+	// GPIOA->OSPEEDR 	|= (3<<2*2);	// PA2 Output speed (100MHz High speed)
+	// GPIOA->OTYPER	&= ~(1<<2);	// PA2 Output type push-pull (reset state)
+	// GPIOA->AFR[0]	|= (2<<8); 	// 0x00000200	(AFR[0].(11~8)=0b0010): Connect TIM5 pins(PA2) to AF2(TIM3..5)
+	// 				// PA2 ==> TIM5_CH3
 
 	// Assign 'Interrupt Period' and 'Output Pulse Period'
-	TIM5->PSC = 840-1;	// Prescaler 84MHz/840 = 0.1MHz (10us)
-	TIM5->ARR = 50000-1;	// Auto reload  : 10us * 50K = 500ms(period)
+	TIM3->PSC = 8400-1;	// Prescaler 84MHz/840 = 0.1MHz (10us)
+	TIM3->ARR = 4000-1;	// Auto reload  : 10us * 50K = 500ms(period)
 
 	// CR1 : Up counting
-	TIM5->CR1 &= ~(1<<4);	// DIR=0(Up counter)(reset state)
-	TIM5->CR1 &= ~(1<<1);	// UDIS=0(Update event Enabled): By one of following events
+	TIM3->CR1 &= ~(1<<4);	// DIR=0(Up counter)(reset state)
+	TIM3->CR1 &= ~(1<<1);	// UDIS=0(Update event Enabled): By one of following events
 				//	- Counter Overflow/Underflow, 
 				// 	- Setting the UG bit Set,
 				//	- Update Generation through the slave mode controller 
-	TIM5->CR1 &= ~(1<<2);	// URS=0(Update event source Selection): one of following events
+	TIM3->CR1 &= ~(1<<2);	// URS=0(Update event source Selection): one of following events
 				//	- Counter Overflow/Underflow, 
 				// 	- Setting the UG bit Set,
 				//	- Update Generation through the slave mode controller 
-	TIM5->CR1 &= ~(1<<3);	// OPM=0(The counter is NOT stopped at update event) (reset state)
-	TIM5->CR1 &= ~(1<<7);	// ARPE=0(ARR is NOT buffered) (reset state)
-	TIM5->CR1 &= ~(3<<8); 	// CKD(Clock division)=00(reset state)
-	TIM5->CR1 &= ~(3<<5); 	// CMS(Center-aligned mode Sel)=00 (Edge-aligned mode) (reset state)
+	TIM3->CR1 &= ~(1<<3);	// OPM=0(The counter is NOT stopped at update event) (reset state)
+	TIM3->CR1 &= ~(1<<7);	// ARPE=0(ARR is NOT buffered) (reset state)
+	TIM3->CR1 &= ~(3<<8); 	// CKD(Clock division)=00(reset state)
+	TIM3->CR1 &= ~(3<<5); 	// CMS(Center-aligned mode Sel)=00 (Edge-aligned mode) (reset state)
 				// Center-aligned mode: The counter counts Up and DOWN alternatively
 
 	// Event & Interrup Enable : UI  
-	TIM5->EGR |= (1<<0);    // UG: Update generation    
+	TIM3->EGR |= (1<<0);    // UG: Update generation    
 
 	////////////////////////////////
-	// Disable Tim5 Update interrupt
+	// Disable TIM3 Update interrupt
     
 	// Define the corresponding pin by 'Output'  
-	TIM5->CCER |= (1<<8);	// CC3E=1: CC3 channel Output Enable
-				// OC3(TIM5_CH3) Active: 해당핀을 통해 신호출력
-	TIM5->CCER &= ~(1<<9);	// CC3P=0: CC3 channel Output Polarity (OCPolarity_High : OC3으로 반전없이 출력)  
+	TIM3->CCER |= (1<<8);	// CC3E=1: CC3 channel Output Enable
+				// OC3(TIM3_CH3) Active: 해당핀을 통해 신호출력
+	TIM3->CCER |= (1<<1);	// CC1P active high
 
 	// 'Mode' Selection : Output mode, toggle  
-	TIM5->CCMR2 &= ~(3<<0); // CC3S(CC3 channel) = '0b00' : Output 
-	TIM5->CCMR2 &= ~(1<<3); // OC3P=0: Output Compare 3 preload disable
-	TIM5->CCMR2 |= (3<<4);	// OC3M=0b011: Output Compare 3 Mode : toggle
+	TIM3->CCMR1 &= ~(3<<0); // CC3S(CC3 channel) = '0b00' : Output 
+	TIM3->CCMR1 &= ~(1<<3); // OC3P=0: Output Compare 3 preload disable
+	TIM3->CCMR1 |= (3<<4);	// OC3M=0b011: Output Compare 3 Mode : toggle
 				// OC3REF toggles when CNT = CCR3
 
- 	TIM5->CCR3 = 30000;	// TIM5 CCR3 TIM5_Pulse
+ 	TIM3->CCR3 = 1000;	// TIM3 CCR3 TIM3_Pulse
 
 	////////////////////////////////
-	// Disable Tim5 CC3 interrupt
+	// Disable TIM3 CC3 interrupt
       
-	TIM5->CR1 |= (1<<0);	// CEN: Enable the Tim5 Counter  					
+	TIM3->CR1 |= (1<<0);	// CEN: Enable the TIM3 Counter  					
 }
 
 void _GPIO_Init(void)
 {
 	// LED (GPIO G) 설정
-    	RCC->AHB1ENR	|=  0x00000040;	// RCC_AHB1ENR : GPIOG(bit#6) Enable							
+    RCC->AHB1ENR	|=  0x00000040;	// RCC_AHB1ENR : GPIOG(bit#6) Enable							
 	GPIOG->MODER 	|=  0x00005555;	// GPIOG 0~7 : Output mode (0b01)						
 	GPIOG->OTYPER	&= ~0x00FF;	// GPIOG 0~7 : Push-pull  (GP8~15:reset state)	
  	GPIOG->OSPEEDR 	|=  0x00005555;	// GPIOG 0~7 : Output speed 25MHZ Medium speed 
@@ -204,9 +204,8 @@ void DisplayTitle(void)
 	LCD_SetFont(&Gulim10);		//폰트 
 	LCD_SetBackColor(RGB_GREEN);	//글자배경색
 	LCD_SetTextColor(RGB_BLACK);	//글자색
-       	LCD_DisplayText(0,0,"ADC1: 0000");
-      	LCD_DisplayText(1,0,"Voltage: 0.00V");
-
+    LCD_DisplayText(0,0,"ADC1: 0000");
+    LCD_DisplayText(1,0,"Voltage: 0.00V");
 	LCD_SetBackColor(RGB_YELLOW);	//글자배경색
 }
 
